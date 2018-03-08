277b0b0ae4a74c5c1747f8b0ed798f4a8992a5b4
MartinFeineis <martifein@gmail.com>
2018-03-08 07:04:26 -0500

    switched to supervisord

--- build_d.sh
+++ build_d.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-zip -r app.zip CPN/* nginx.conf uwsgi.ini emperor.uwsgi.service requirements.txt orch.sh
+zip -r app.zip CPN/* nginx.conf uwsgi.ini requirements.txt orch.sh supervisord.conf
 sudo docker build --iidfile=image.id .
 rm -f cont.id
-sudo docker run -d -i -p 8090:80 -t $(cat image.id) nginx -g 'daemon off;' --cidfile=cont.id
-#sudo docker run -i --cidfile=cont.id  -p 8090:80 -t $(cat image.id) /bin/bash
+#sudo docker run -d -i -p 8090:80 -t $(cat image.id) nginx -g 'daemon off;' --cidfile=cont.id
+sudo docker run -i --cidfile=cont.id  -p 8090:80 -t $(cat image.id) /bin/bash
--- orch.sh
+++ orch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 apt-get update ; apt-get install --fix-missing
-apt-get install -y build-essential python-dev nginx python3 python3-pip zip systemd
+apt-get install -y build-essential python-dev nginx python3 python3-pip zip supervisor
 pip3 install --upgrade pip
 pip3 install --no-cache-dir -r requirements.txt
-cp emperor.uwsgi.service /etc/systemd/system/emperor.uwsgi.service
 cp nginx.conf /etc/nginx/nginx.conf
+cp supervisord.conf /etc/supervisor/supervisord.conf
 update-rc.d nginx enable
-service nginx start
+#service nginx start
 cd /var/www/CPN
-uwsgi --socket :8000 --wsgi-file CPN/wsgi.py
+#uwsgi --socket :8000 --wsgi-file CPN/wsgi.py
