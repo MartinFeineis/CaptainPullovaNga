4cc710ee6bb328eeb16f6d1ce17c67193edcc8e5
MartinFeineis <martifein@gmail.com>
2018-03-10 11:17:52 -0500

    Everything works now

--- build_d.sh
+++ build_d.sh
@@ -3,5 +3,5 @@
 zip -r app.zip CPN/* nginx.conf uwsgi.ini requirements.txt orch.sh supervisord.conf
 sudo docker build --iidfile=image.id .
 rm -f cont.id
-#sudo docker run -d -i -p 8090:80 -t $(cat image.id) nginx -g 'daemon off;' --cidfile=cont.id
-sudo docker run -i --cidfile=cont.id  -p 8090:80 -t $(cat image.id) /bin/bash
+sudo docker run -d -i -p 8090:80 --cidfile=cont.id -t $(cat image.id) supervisord
+#sudo docker run -i --cidfile=cont.id  -p 8090:80 -t $(cat image.id) /bin/bash
